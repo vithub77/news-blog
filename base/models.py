a0ff9ba81a489776cbc380796a7b56eb3e57a963
Vitaly Lebedev <vitalylebedev88@gmail.com>
2024-05-12 21:55:11 +0300

    create migration

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsBd(models.Model):
+    source = models.CharField(max_length=500)
+    description = models.TextField(default='')
+    url = models.CharField(max_length=500)
+    data = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.source, self.description
+
+
+class Comments(models.Model):
+    news = models.ForeignKey(NewsBd, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    text = models.TextField(default='')
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author, self.text
