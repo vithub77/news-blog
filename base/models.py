0e73374677f1aabe375dec3f4bc1db69b66410cc
Vitaly Lebedev <vitalylebedev88@gmail.com>
2024-05-13 17:16:14 +0300

    func class News

--- base/models.py
+++ base/models.py
@@ -8,7 +8,7 @@ class NewsBd(models.Model):
     data = models.DateTimeField(null=True)
 
     def __str__(self):
-        return self.source, self.description
+        return self.url
 
 
 class Comments(models.Model):
@@ -18,4 +18,24 @@ class Comments(models.Model):
     publication_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.author, self.text
+        return self.author
+
+
+class NewsBdEn(models.Model):
+    source = models.CharField(max_length=500)
+    description = models.TextField(default='')
+    url = models.CharField(max_length=500)
+    data = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.url
+
+
+class CommentsEn(models.Model):
+    news = models.ForeignKey(NewsBd, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    text = models.TextField(default='')
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author
--- base/news.py
+++ base/news.py
@@ -1,6 +1,8 @@
+from django.utils import timezone
 from newsapi import NewsApiClient
 import datetime as dt
 
+from .models import NewsBd
 from secret import api_key
 
 
@@ -13,6 +15,7 @@ class News:
     _api = NewsApiClient
     _time_ru = dt
     _time_en = dt
+    _time_update_news = 0  # minutes
 
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
@@ -22,21 +25,38 @@ class News:
             cls.__instance._time_en = dt.datetime.now()
         return cls.__instance
 
-    def get_news(self, news_ru):
+    def get_news(self, news_ru: bool):
         if news_ru:
-            if (dt.datetime.now() - dt.timedelta(minutes=1)) < self._time_ru:
-                print(self._time_ru, 'self._time_ru')
+            if (dt.datetime.now() - dt.timedelta(minutes=self._time_update_news)) < self._time_ru:
                 return self._list_news_ru
             else:
-                r = self._api.get_top_headlines(language='ru')
-                self._list_news_ru = r.get('articles')
-                return self._list_news_ru
+                self._time_ru = dt.datetime.now()
+                news_json = self._api.get_top_headlines(language='ru')
+                self._set_news_bd(news_json.get('articles'), news_ru)
+
         else:
-            if (dt.datetime.now() - dt.timedelta(minutes=20)) < self._time_en:
+            if (dt.datetime.now() - dt.timedelta(minutes=self._time_update_news)) < self._time_en:
                 return self._list_news_en
             else:
-                r = self._api.get_top_headlines(language='en')
-                self._list_news_en = r.get('articles')
-                return self._list_news_en
+                self._time_en = dt.datetime.now()
+                news_json_en = self._api.get_top_headlines(language='en')
+                self._set_news_bd(news_json_en.get('articles'), news_ru)
 
+    def _set_news_bd(self, news, news_ru: bool):
+        l_time = [f"{(i.data - dt.timedelta(hours=3)).strftime('%Y-%m-%d %H:%M:%S')}" for i in NewsBd.objects.order_by('id')[:20]]
+        for n in news:
+            published_time = n.get('publishedAt')[:-1].replace('T', ' ')
+            if published_time in l_time:
+                continue
+            t = n.get('title').split('-')
+            author = t[-1].strip()
+            description = ' '.join(t[:-1])
+            url = n.get('url')
+            published_time = dt.datetime.strptime(published_time, "%Y-%m-%d %H:%M:%S") + dt.timedelta(hours=3)
+            published_time = timezone.make_aware(published_time)
+            _news = NewsBd(source=author, description=description,
+                           url=url, data=published_time)
+            _news.save()
 
+    def _get_from_bd(self):
+        pass
