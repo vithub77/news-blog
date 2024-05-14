2708fa011850aa68698980032c930cad48d23813
Vitaly Lebedev <vitalylebedev88@gmail.com>
2024-05-14 22:01:35 +0300

    add ClassView, registerHtml

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = 'base'
 urlpatterns = [
     path('', index, name='index'),
-    path('news/<str:lang>/', page_news, name='page_news'),
+    path('news/<str:lang>/', MainPage.as_view(), name='main_page'),
+    path('news/<str:lang>/username', Login.as_view(), name='login'),
+    path('news/<str:lang>/register/', Register.as_view(), name='register'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,17 +1,65 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 
 from .news import News
 
 
 def index(request):
-    url_redirect = reverse('base:page_news', args=('ru',))
+    url_redirect = reverse('base:main_page', args=('ru',))
     return redirect(url_redirect, permanent=True)
 
 
-def page_news(request, lang):
+class MainPage(generic.ListView):
     news = News()
-    list_news = news.get_news(news_ru=True if lang == 'ru' else False)
-    return render(request, 'base/news.html', {'list_news': list_news,
-                                              'title': 'Новости'
-                                              })
+    template_name = "base/news.html"
+    context_object_name = "list_news"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        lang = self.kwargs.get('lang', 'ru')
+        context['title'] = 'Новости России' if lang == 'ru' else 'Новости в Мире'
+        context['lang'] = lang
+        return context
+
+    def get_queryset(self):
+        return self.news.get_news(news_ru=True if self.kwargs['lang'] == 'ru' else False)
+
+
+class Register(generic.ListView):
+    news = News()
+    template_name = "base/register.html"
+    context_object_name = "list_news"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        lang = self.kwargs.get('lang', 'ru')
+        context['title'] = 'Новости России-Регистрация' if lang == 'ru' else 'Новости в Мире-Регистрация'
+        context['lang'] = lang
+        return context
+
+    def get_queryset(self):
+        return self.news.get_news(news_ru=True if self.kwargs['lang'] == 'ru' else False)
+
+
+# def register(request, lang):
+#     # return HttpResponse('страница Рега')
+#     # form_class = UserCreationForm
+#     return render(request, 'base/register.html')
+
+class Login(generic.ListView):
+    news = News()
+    template_name = "base/user.html"
+    context_object_name = "list_news"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        lang = self.kwargs.get('lang', 'ru')
+        context['title'] = 'Новости России' if lang == 'ru' else 'Новости в Мире'
+        context['lang'] = lang
+        return context
+
+    def get_queryset(self):
+        return self.news.get_news(news_ru=True if self.kwargs['lang'] == 'ru' else False)
